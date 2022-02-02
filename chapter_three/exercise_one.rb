0b807e469d005e0129cc0755b6b2a2c5d31d8c53
Jensen Taylor <78d9f80a@slack.launchschool.com>
2022-02-02 08:49:32 -0800

    chapter_three_exercises

--- chapter_three/exercise_one.rb
+++ chapter_three/exercise_one.rb
@@ -1,3 +1,6 @@
+# Write a program that prints a message. Method name is called greeting that takes a parameter
+# called name. Then returns as a string.
+
 def greeting(name)
   "Hello " + name + ", how are you?"
 end
