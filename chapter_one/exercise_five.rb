b494f2413d77e3670d155ac6608888263b7d06a7
Jensen Taylor <78d9f80a@slack.launchschool.com>
2022-02-02 08:41:38 -0800

    chapter_one_exercises

--- chapter_one/exercise_five.rb
+++ chapter_one/exercise_five.rb
@@ -1,4 +1,4 @@
-# Write a program that outputs the factorial of numbers 5, 6, 7, and 8.
+# Write a program that will output the factorial of numbers 5, 6, 7, and 8.
 # Assigned each to a variable, then output the result using those variables.
 
 a = 5 * 4 * 3 * 2 * 1
